296c053e86a7835556fdbeaf3b8295375127b085
Maxim <Maxim@172.18.18.71>
2018-04-06 16:15:15 +0200

    Commit3

--- SandBox/src/fr/imta/prime/cacao2018/sandbox/SandBox.java
+++ SandBox/src/fr/imta/prime/cacao2018/sandbox/SandBox.java
@@ -3,7 +3,7 @@ package fr.imta.prime.cacao2018.sandbox;
 public class SandBox {
 
 	public static void main(String[] args) {
-		System.out.println("Hello A");
+		System.out.println("Hello B");
 	}
 
 }
